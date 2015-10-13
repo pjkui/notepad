af8cdd2f2adeb50d8b7393ce0a0b4a1a7c3ce235
pjkui <pjkui@qq.com>
2015-10-13 20:00:47 +0800

    add note app

---
+++ note/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class  Note(models.Model):
+	title=models.CharField(max_length=60)
+	color=models.CharField(max_length=10)
+	author_id=models.IntegerField()
+	noteType=models.IntegerField(max_length=10)
+	createTime=models.DateTimeField()
+	publish=models.DateTimeField()
+	content=models.CharField()
+
+class User(models.Model):
+	username=models.CharField(max_length=40)
+	password=models.CharField(max_length=40)
+	nickname=models.CharField(max_length=40)
+	loginIP=models.CharField(max_length=12)
+
+class NoteAuthor(models.Model):
+	userID=models.ForeignKey(User)
+	noteID=models.ForeignKey(Note)
