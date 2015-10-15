d8098b462094f0c967dfd00c18424d21cee5145f
Quinn Pan <pjkui@qq.com>
2015-10-15 21:57:13 +0800

    add form , support bootstrap

--- note/admin.py
+++ note/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from note.models import Note
+from note.models import User
+from note.models import NoteAuthor
+
+admin.site.register(Note)
+admin.site.register(NoteAuthor)
+admin.site.register(User)
--- notepad/urls.py
+++ notepad/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+# from note import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    #router to index
+    url(r'^note/','note.views.index', name='index'),
 ]
