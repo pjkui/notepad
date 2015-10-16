77f226b685f851afbc77560b6823376760286aca
Quinn Pan <pjkui@qq.com>
2015-10-16 23:53:00 +0800

    add form files

---
+++ note/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+class AddForm(forms.Form):
+	# """docstring for AddNote"""
+	title = forms.CharField(max_length=40)
+	color = forms.CharField(max_length=10)
+	# author_id = forms.IntegerField()
+	# noteType = forms.IntegerField()
+	# createTime = forms.DateTimeField()
+	# publish = forms.DateTimeField()
+	# content_text = forms.CharField(widget=forms.Textarea())
+	# class Meta:
+	# 	model = Note
+	# 	fields = ('title','color','author_id','noteType','content')
+	# 	widgets = {
+	# 		'name': Textarea(attrs={'cols': 80, 'rows': 20}),
+	# 	}
---
+++ note/migrations/0001_initial.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Note',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=60)),
+                ('color', models.CharField(max_length=10)),
+                ('author_id', models.IntegerField()),
+                ('noteType', models.IntegerField()),
+                ('createTime', models.DateTimeField()),
+                ('publish', models.DateTimeField()),
+                ('content', models.TextField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='NoteAuthor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('noteID', models.ForeignKey(to='note.Note')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='User',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('username', models.CharField(max_length=40)),
+                ('password', models.CharField(max_length=40)),
+                ('nickname', models.CharField(max_length=40)),
+                ('loginIP', models.CharField(max_length=12)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='noteauthor',
+            name='userID',
+            field=models.ForeignKey(to='note.User'),
+        ),
+    ]
